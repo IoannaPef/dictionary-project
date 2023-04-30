69e3bdc85e0c196d45b6c33b74246a2d56e5f819
IoannaKara <ioannakara2009@hotmail.com>
2023-04-30 22:55:37 +0200

    added the photos component and styled it

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultWord);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
     setResults(response.data[0]);
   }
 
+  function handlePhotosResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey = "1f5fdcf1ceb4ff37fcotf403ad4e9bc8";
+    let pexelsApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${pexelsApiKey}`;
+    
+    axios.get(pexelsApiUrl).then(handlePhotosResponse);
   }
 
   function handleSubmit(event) {
@@ -44,6 +53,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
